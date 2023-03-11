9d8582a932b2c7e14af579ce6dd7b8354c2ec7db
scherbak <scherbak.pavel5@yandex.ru>
2023-03-11 13:59:18 +0400

    Делаем фронт для телеграм бота.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,124 @@
-import Counter from "./components/Counter";
+import './App.css';
+import React, {useEffect, useState} from "react";
 
 function App() {
+    let baseUrl = "http://localhost:8080";
+    let tg = window.Telegram.WebApp;
+    let user = tg.initDataUnsafe?.user;
+    let userId = user?.id;
+    userId = user?.id ? user?.id : 1;
+
+    const [totalSum, setTotalSum] = useState(0);
+    const [inSum, setInSum] = useState(0);
+    const [outSum, setOutSum] = useState(0);
+    const [sum, setSum] = useState('');
+    const [category, setCategory] = useState('');
+    const [operations, setOperations] = useState([]);
+
+    const onSendData = (e) => {
+        e.preventDefault();
+        if (!sum || !category) {
+            alert("Заполните данные!");
+            return;
+        }
+        fetch(baseUrl + "/api/operation", {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                userId: userId,
+                sum: sum,
+                category: category
+            })
+        }).then(() => {
+            setSum('');
+            setCategory('');
+            loadInfo();
+            loadHistory();
+        });
+    }
+
+    function loadInfo() {
+        fetch(baseUrl + '/api/info?userId=' + userId)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setTotalSum(data.totalSum);
+                setInSum(data.inSum);
+                setOutSum(data.outSum);
+            })
+    }
+
+    function loadHistory() {
+        fetch(baseUrl + '/api/operation-history?userId=' + userId)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                setOperations(data)
+            })
+    }
+
+    useEffect(() => {
+        loadInfo();
+        loadHistory();
+    }, []);
+
     return (
         <div className="App">
-            <Counter/>
+            <div>Фамилия: {user?.last_name}</div>
+            <div>Имя: {user?.first_name}</div>
+            <br/>
+            <div>Баланс: {totalSum} руб.</div>
+            <div>Доход: {inSum} руб.</div>
+            <div>Расход: {outSum} руб.</div>
+            <br/>
+            <h3>Введите данные операции:</h3>
+            <form>
+                <div className="mb-3">
+                    <label className="form-label">Сумма</label>
+                    <input
+                        className="form-control"
+                        type="number"
+                        value={sum}
+                        onChange={(e) => setSum(e.target.value)}
+                    />
+                </div>
+                <div className="mb-3">
+                    <label className="form-label">Категория</label>
+                    <input
+                        className="form-control"
+                        type="text"
+                        value={category}
+                        onChange={(e) => setCategory(e.target.value)}
+                    />
+                </div>
+                <button className="btn btn-success" onClick={onSendData}>Сохранить</button>
+            </form>
+            <h3>Последнии 5 операций:</h3>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th scope="col">ID</th>
+                    <th scope="col">Когда</th>
+                    <th scope="col">Сумма</th>
+                    <th scope="col">Категория</th>
+                </tr>
+                </thead>
+                <tbody>
+                {operations.map(
+                    (operation, i) =>
+                        <tr key={i}>
+                            <td>{operation.id}</td>
+                            <td>{operation.createdAt}</td>
+                            <td>{operation.sum}</td>
+                            <td>{operation.category}</td>
+                        </tr>
+                )}
+                </tbody>
+            </table>
         </div>
     );
 }
